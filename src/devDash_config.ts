0574438501645e04faf941521b9e5fdcbf0219b9
jssatorres <juansataz.dev@gmail.com>
2023-01-21 14:12:21 +0100

    Feat (devDash) github repository

--- src/devDash_config.ts
+++ src/devDash_config.ts
@@ -9,17 +9,17 @@ export interface DevDashConfig {
 export const config: DevDashConfig = {
 	github_access_token: process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN as string,
 	widgets: [
-		// {
-		// 	id: "2565fa91-2ac4-4e4f-9111-6d27a598082d",
-		// 	repository_url: "https://github.com/CodelyTV/dotly",
-		// },
+		{
+			id: "2565fa91-2ac4-4e4f-9111-6d27a598082d",
+			repository_url: "https://github.com/JSSatorres/React-Clients",
+		},
 		{
 			id: "a66d5092-5ba6-4184-9931-cc485defe412",
+			repository_url: "https://github.com/JSSatorres/coffe-shop",
+		},
+		{
+			id: "7c7a6b71-76dc-42ce-a46b-1730fc758193",
 			repository_url: "https://github.com/JSSatorres/vertical-menu-typescript-sass",
 		},
-		// {
-		// 	id: "7c7a6b71-76dc-42ce-a46b-1730fc758193",
-		// 	repository_url: "https://github.com/CodelyTV/refactoring-code-smells",
-		// },
 	],
 };
